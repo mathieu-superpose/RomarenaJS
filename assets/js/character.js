920c3e5c47b3fd892744824db7a50b7823ea0ee7
Mathieu <mathieu.joly@lrguas.ch>
2021-01-06 23:34:24 +0100

    add Fighter inherited class

--- assets/js/character.js
+++ assets/js/character.js
@@ -6,3 +6,14 @@ class Character {
         this.status = status;
     }
 }
+
+class Fighter extends Character {
+    constructor(name, hp = 12, dmg = 4, mana = 160, status) {
+        super(hp, dmg, mana, status);
+        this.name = name;
+
+        //Le Fighter aura une attaque spéciale Dark Vision, infligeant 5 dégâts.
+        //Jusqu'au prochain tour, il prendra 2 dégâts de moins par coup reçu. 
+        //Elle coute 20 mana.
+    }
+}
